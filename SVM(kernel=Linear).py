8afe0d581aa5b7792c9cfea7199e602b4760904e
hamedtajik <71550944+hamedtajik@users.noreply.github.com>
2021-10-22 17:11:21 +0200

    SVM(A-Z)

---
+++ SVM(kernel=Linear).py
@@ -0,0 +1,40 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+data = pd.read_csv(r'C:\Users\Hamed\Downloads\Machine Learning A-Z (Codes and Datasets)\Part 3 - Classification\Section 14 - Logistic Regression\Python\Social_Network_Ads.csv')
+X = data.iloc[:, :-1].values
+y = data.iloc[:, -1].values
+
+'''
+split train-set and test-set
+'''
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 1)
+
+
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+print(X_train)
+print(X_test)
+
+# Training the SVM model on the Training set
+from sklearn.svm import SVC
+classifier = SVC(kernel = 'linear', random_state = 0)
+classifier.fit(X_train, y_train)
+# Predicting a new result
+prediction = print(classifier.predict(sc.transform([[30,87000]])))
+#
+# Predicting the Test set results
+y_pred = classifier.predict(X_test)
+print(np.concatenate((y_pred.reshape(len(y_pred),1), y_test.reshape(len(y_test),1)),1))
+
+# Making the Confusion Matrix
+from sklearn.metrics import confusion_matrix, accuracy_score
+cm = confusion_matrix(y_test, y_pred)
+print(cm)
+acs = accuracy_score(y_test, y_pred)
+print(acs)
+
